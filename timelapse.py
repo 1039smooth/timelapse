c4ee1c9136287e640950c55eb5a6f92215fa42a2
vynce <vsex10@gmail.com>
2016-12-01 22:14:31 -0800

    removing whitespace

--- timelapse.py
+++ timelapse.py
@@ -29,16 +29,15 @@ while True:
         Min = "%02d" % (d.minute)
         Sec = "%02d" % (d.second)
         print "Say Cheese! ---------------------------------------->"
-        
-        
+
         os.system("fswebcam -r 1280x720 --no-banner -D 1 " + str(Hour) + ":" + str(Min) + ":" + str(Sec) + "_" + str(Month) + "." + str(Day) + "." + str(Year) + "jpg")
         #-D 1 (adds 1 sec delay)
 
         count += 1
         time.sleep(28)
-      
+
     else:
-        
+
         print " ******************************************** Exiting"
         sys.exit()
 		
